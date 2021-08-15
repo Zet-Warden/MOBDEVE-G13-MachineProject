6772e4dd8f9e9f1d4965a386f6764460159be4ee
Zet-Warden <74450482+Zet-Warden@users.noreply.github.com>
2021-08-15 15:34:44 +0800

    added comments on UI Helper class

--- app/src/main/java/com/mobdeve/s11/sy/jacob/machineproject/UIHider.kt
+++ app/src/main/java/com/mobdeve/s11/sy/jacob/machineproject/UIHider.kt
@@ -1,18 +1,25 @@
+@file:Suppress("DEPRECATION")
+
 package com.mobdeve.s11.sy.jacob.machineproject
 
 import android.os.CountDownTimer
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
-import kotlinx.android.synthetic.main.activity_main.*
 
+/**
+ * Creates an object that hides System status and navigation bar
+ * If the user opts to make the status and navigation bar appear again, it waits for a certain time
+ * then hides the System status and navigation bar again
+ */
 class UIHider(
     private val app: AppCompatActivity,
-    private val clMainContainer: ConstraintLayout
-) : CountDownTimer(1500, 1000) {
+    clMainContainer: View
+) : CountDownTimer(1500, 1000) { //set wait time to 1.5s when user opts to make the status and navigation bar appear again
 
     init {
         hideSystemUI()
+
+        //listen to the main container of the activity to check whether the status and navigation bar shows up
         clMainContainer.setOnClickListener {
             this.cancel()
             this.start()
@@ -21,10 +28,16 @@ class UIHider(
 
     override fun onTick(millisUntilFinished: Long) {}
 
+    /**
+     * Hides the System UI when timer is finish
+     */
     override fun onFinish() {
         hideSystemUI()
     }
 
+    /**
+     * Hides the System UI
+     */
     private fun hideSystemUI() {
         app.window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
                 or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
