f0454f6e17d7de0bbdf9d18e64d3ea0987b43c35
Zet-Warden <74450482+Zet-Warden@users.noreply.github.com>
2021-08-15 17:46:01 +0800

    change phone field to be editable

--- app/src/main/java/com/mobdeve/s11/sy/jacob/machineproject/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s11/sy/jacob/machineproject/MainActivity.kt
@@ -5,7 +5,10 @@ package com.mobdeve.s11.sy.jacob.machineproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.StringBuilder
 
 
 class MainActivity : AppCompatActivity() {
@@ -17,5 +20,46 @@ class MainActivity : AppCompatActivity() {
         btnSendOTP.setOnClickListener {
             startActivity(Intent(this, OTPVerification::class.java))
         }
+
+        etMobileNumberInput.addTextChangedListener(object : TextWatcher {
+            var flag = true
+            var prevLength: Int? = 0
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                if (flag && !isDeleting(prevLength, s?.length)) {
+                    flag = false
+                    s?.replace( //this triggers afterTextChanged event, flag is necessary to avoid infinite recursions
+                        0,
+                        s.length,
+                        formatString(s)
+                    )
+                }
+                prevLength = s?.length
+                flag = true
+            }
+
+            private fun formatString(s: Editable?): String {
+                val sb = StringBuilder()
+                s?.forEachIndexed { index, c ->
+                    if (c != ' ') { //skip spaces, treat s as a continuous char of digits
+                        sb.append(c)
+                        if (index == 2 || index == 6)
+                            sb.append(' ')
+                    }
+                }
+                return sb.toString()
+            }
+
+            private fun isDeleting(prevLength: Int?, currLength: Int?): Boolean {
+                if (prevLength != null && currLength != null)
+                    return prevLength > currLength
+                return false
+            }
+
+        })
     }
 }
