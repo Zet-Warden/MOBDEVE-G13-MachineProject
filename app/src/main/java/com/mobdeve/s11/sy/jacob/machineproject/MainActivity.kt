ed625ffc1b9fa421b616b1b561f132e8b486325d
Zet-Warden <74450482+Zet-Warden@users.noreply.github.com>
2021-08-15 15:37:51 +0800

    changed UIHider constructor parameter

--- app/src/main/java/com/mobdeve/s11/sy/jacob/machineproject/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s11/sy/jacob/machineproject/MainActivity.kt
@@ -5,26 +5,17 @@ package com.mobdeve.s11.sy.jacob.machineproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.CountDownTimer
-import android.view.View
-
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var hideUITimer: CountDownTimer
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         UIHider(this, clMainContainer)
 
-
-
         btnSendOTP.setOnClickListener {
             startActivity(Intent(this, OTPVerification::class.java))
         }
     }
-
-
 }
--- app/src/main/java/com/mobdeve/s11/sy/jacob/machineproject/UIHider.kt
+++ app/src/main/java/com/mobdeve/s11/sy/jacob/machineproject/UIHider.kt
@@ -2,17 +2,21 @@
 
 package com.mobdeve.s11.sy.jacob.machineproject
 
+import android.app.Activity
 import android.os.CountDownTimer
 import android.view.View
-import androidx.appcompat.app.AppCompatActivity
 
 /**
  * Creates an object that hides System status and navigation bar
  * If the user opts to make the status and navigation bar appear again, it waits for a certain time
  * then hides the System status and navigation bar again
+ *
+ * Parameters:
+ * app : AppCompatActivity - the activity that will hide the System UI
+ * clMainContainer : View - the main container of the activity
  */
 class UIHider(
-    private val app: AppCompatActivity,
+    private val app: Activity,
     clMainContainer: View
 ) : CountDownTimer(1500, 1000) { //set wait time to 1.5s when user opts to make the status and navigation bar appear again
 
