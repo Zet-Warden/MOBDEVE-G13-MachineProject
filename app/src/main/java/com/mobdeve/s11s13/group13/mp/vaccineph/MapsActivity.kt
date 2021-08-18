bb4ef5b4f592faf47ad02856a12291bf566e897b
WZT006 <60822592+WZT006@users.noreply.github.com>
2021-08-18 11:50:28 +0800

    Variable Pinning

--- app/src/main/java/com/mobdeve/s11s13/group13/mp/vaccineph/MapsActivity.kt
+++ app/src/main/java/com/mobdeve/s11s13/group13/mp/vaccineph/MapsActivity.kt
@@ -4,6 +4,7 @@ import android.location.Geocoder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.fragment.app.FragmentManager
 import com.google.android.gms.maps.CameraUpdateFactory
 
 
@@ -16,11 +17,15 @@ import com.google.android.gms.maps.GoogleMap.OnCameraIdleListener
 import com.google.android.gms.maps.GoogleMap.OnCameraMoveCanceledListener
 import com.google.android.gms.maps.GoogleMap.OnCameraMoveListener
 import com.google.android.gms.maps.GoogleMap.OnCameraMoveStartedListener
+import com.google.firebase.firestore.FirebaseFirestore
 
 
 class MapsActivity : AppCompatActivity() {
 
-
+    private val db = FirebaseFirestore.getInstance()
+    private val collectionName = "vaccination centers"
+    private var lat = 15.6037
+    private var long = 120.9737
 
 
 
@@ -28,10 +33,11 @@ class MapsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
 
-
+//        readDbfromCoords("2401 Taft Ave, Malate, Manila, 1004 Metro Manila")
         //Get handle and register callback
 
 
     }
+
 
 }
--- app/src/main/java/com/mobdeve/s11s13/group13/mp/vaccineph/MapsFragment.kt
+++ app/src/main/java/com/mobdeve/s11s13/group13/mp/vaccineph/MapsFragment.kt
@@ -3,9 +3,11 @@ package com.mobdeve.s11s13.group13.mp.vaccineph
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -13,8 +15,20 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.GeoPoint
 
 class MapsFragment : Fragment() {
+
+
+    private val db = FirebaseFirestore.getInstance()
+    private val collectionName = "vaccination centers"
+    private var lat = 0.0
+    private var long = 0.0
+    private lateinit var map : GoogleMap
+
+
+
 
     private val callback = OnMapReadyCallback { googleMap ->
         /**
@@ -26,27 +40,24 @@ class MapsFragment : Fragment() {
          * install it inside the SupportMapFragment. This method will only be triggered once the
          * user has installed Google Play services and returned to the app.
          */
-
+        map = googleMap
         googleMap.setMinZoomPreference(15.0f)
         googleMap.setMaxZoomPreference(20.0f)
 
-        //TODO Change values of lat and long to read from db
-        val lat = 14.5585560991
-        val long = 120.989571042
-        googleMap.animateCamera(CameraUpdateFactory.zoomTo(20.0f))
-        val PH = LatLng(lat,long)
+        //TODO Implement GetAssignedCenter ( gets which vacc center user is assigned to)
 
-        googleMap.addMarker(MarkerOptions().position(PH).title("Marker in DLSU"))
-        googleMap.animateCamera(
-            CameraUpdateFactory.newLatLngZoom(
-                LatLng(
-                    lat,
-                    long
-                ), 18.0f
-            )
-        )
+        getAssignedCenter()
+        readDbfromCoords("Robinsons Place Manila")
 
 
+
+
+
+
+    }
+
+    private fun getAssignedCenter() {
+        TODO("Not yet implemented")
     }
 
     override fun onCreateView(
@@ -62,5 +73,40 @@ class MapsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
+    }
+
+
+
+    private fun readDbfromCoords(address : String){
+
+        db.collection(collectionName)
+            .whereEqualTo("name", address)
+            .get()
+            .addOnSuccessListener { query ->
+                if (query.size() > 0)
+                    for (document in query)
+                        if (document.contains("location")) {
+                            lat = document.getGeoPoint("location")?.latitude!!
+                            long = document.getGeoPoint("location")?.longitude!!
+
+                            changecamera()
+                        }
+
+            }
+
+    }
+
+    private fun changecamera() {
+        map.animateCamera(CameraUpdateFactory.zoomTo(20.0f))
+        val coords = LatLng(lat,long)
+        map.addMarker(MarkerOptions().position(coords).title("Vaccine Center"))
+        map.animateCamera(
+            CameraUpdateFactory.newLatLngZoom(
+                LatLng(
+                    lat,
+                    long
+                ), 18.0f
+            )
+        )
     }
 }
