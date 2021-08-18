b2ad32e50c40de9f7e0985ea509d0b5a8bc0d2ce
Zet-Warden <74450482+Zet-Warden@users.noreply.github.com>
2021-08-19 01:36:09 +0800

    made it less buggy

--- app/src/main/java/com/mobdeve/s11s13/group13/mp/vaccineph/AppointmentScreenActivity.kt
+++ app/src/main/java/com/mobdeve/s11s13/group13/mp/vaccineph/AppointmentScreenActivity.kt
@@ -1,14 +1,10 @@
 package com.mobdeve.s11s13.group13.mp.vaccineph
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.tasks.Task
-import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.QuerySnapshot
-import com.google.firebase.firestore.SetOptions
-import com.google.firebase.ktx.Firebase
 import com.mobdeve.s11s13.group13.mp.vaccineph.helpers.UIHider
 import kotlinx.android.synthetic.main.activity_appointment_screen.*
 import kotlinx.android.synthetic.main.activity_appointment_screen.btnSave
@@ -22,8 +18,6 @@ import kotlinx.android.synthetic.main.activity_appointment_screen.btnCalendar
 import kotlinx.android.synthetic.main.activity_appointment_screen.btnHome
 import kotlinx.android.synthetic.main.activity_appointment_screen.btnLocation
 import kotlinx.android.synthetic.main.activity_appointment_screen.btnProfile
-import kotlinx.android.synthetic.main.activity_user_screen.*
-import java.time.LocalTime
 
 class AppointmentScreenActivity : AppCompatActivity() {
 
@@ -41,13 +35,13 @@ class AppointmentScreenActivity : AppCompatActivity() {
         updateCalendarView()
     }
 
+    @SuppressLint("SetTextI18n")
     private fun updateCalendarView() {
        getSavedDate {
            println(it)
            if(it != null) {
                val date = it.toDateOrNull()
                if(date != null) {
-
                    cvCalendar.setDate(date.time, true, true)
                    tvAppointmentDate.text = it
                } else {
@@ -69,17 +63,9 @@ class AppointmentScreenActivity : AppCompatActivity() {
         initCalendar()
         initSaveButton()
         getMaxCap()
-        //getMaxCap() //get maximum capacity of vaxx center
-        //getSlotsTaken(Calendar().getXDaysFromNow(2)) //idk if this should be here actually, but this checks how many slots take
     }
 
     private fun initCalendar() {
-        //initialize the default appointment date
-
-        /*val dayAfterTomorrow = Calendar().getXDaysFromNow(2)
-        cvCalendar.setDate(dayAfterTomorrow.time, true, true)
-        tvAppointmentDate.text = dayAfterTomorrow.toFormattedString()*/
-
         //changes the date when user chooses a new date
         cvCalendar.setOnDateChangeListener { _, year, month, date ->
             val selectedDate = Calendar().createDate(year, month, date)
@@ -92,9 +78,9 @@ class AppointmentScreenActivity : AppCompatActivity() {
         var selectedDate = dayAfterTomorrow
         var prevDate = dayAfterTomorrow
 
-        val saveDateMessage = Toast.makeText(this, "Saved!", Toast.LENGTH_SHORT);
+        val saveDateMessage = Toast.makeText(this, "Saved!", Toast.LENGTH_SHORT)
         val invalidDateMessage =
-            Toast.makeText(this, "Invalid, resetting date", Toast.LENGTH_SHORT);
+            Toast.makeText(this, "Invalid, resetting date", Toast.LENGTH_SHORT)
         val appointmentFullMessage =
             Toast.makeText(this, "Appointment for this day is full already", Toast.LENGTH_LONG)
 
@@ -122,18 +108,6 @@ class AppointmentScreenActivity : AppCompatActivity() {
         }
     }
 
-    private fun amIKachow(myTime : Long, allTime : List<Long>) : Boolean {
-        var fastestTime : Long = Long.MAX_VALUE
-
-        for (time in allTime) {
-            if(time < fastestTime)
-                fastestTime = time
-        }
-
-        return myTime == fastestTime
-    }
-
-
     private fun isValidDate(date: Date): Boolean {
         return date.after(Calendar().getTomorrow())
     }
@@ -143,9 +117,11 @@ class AppointmentScreenActivity : AppCompatActivity() {
         return taken < max
     }
 
-    // gets the maximum capacity of the vaxx center
+    // gets the maximum capacity of the vaccine
+    // center
     private fun getMaxCap() {
-        val location = "dummy location" //use geolocation to determine the closest vaxx center
+        val location = UserData.location //use geolocation to determine the closest vaccine
+        // center
 
         val db = FirebaseFirestore.getInstance()
         db.collection("vaccination centers")
@@ -162,7 +138,8 @@ class AppointmentScreenActivity : AppCompatActivity() {
     private fun getSlotsTaken(d: Date, callback : () -> Unit) {
         val date = d.toFormattedString()
         val db = FirebaseFirestore.getInstance()
-        val location = "dummy location" //use geolocation to determine the closest vaxx center
+        val location = UserData.location //use geolocation to determine the closest vaccine
+        // center
 
         db.collection("appointments")
             .whereEqualTo("date", date)
@@ -180,14 +157,14 @@ class AppointmentScreenActivity : AppCompatActivity() {
     // saves the user's appointment to the database
     private fun saveToDatabase() {
         val db = FirebaseFirestore.getInstance()
-        val location = "dummy location" //use geolocation to determine the closest vaxx center
+        val location = UserData.location //use geolocation to determine the closest vaccine
+        // center
 
         //make a new appointment
         val newAppointment = hashMapOf(
             "date" to tvAppointmentDate.text,
             "location" to location,
             "mobile number" to UserData.mobileNumber,
-            "time stamp" to System.currentTimeMillis()
         )
 
         db.collection("appointments")
@@ -196,27 +173,14 @@ class AppointmentScreenActivity : AppCompatActivity() {
             .addOnSuccessListener { query ->
                 //if user made an appointment before
                 if (query.size() > 0) {
-                    var docId = query.documents[0].id
+                    val docId = query.documents[0].id
                     db.collection("appointments").document(docId)
                         .delete() //delete previous appointment
                 }
 
                 db.collection("appointments")
                     .add(newAppointment) //add new appointment to the database
-                    .addOnSuccessListener {
-                        //if(amIKachow(newAppointment.get("time stamp"), ))
-                    }
-
             }
-    }
-
-    private fun getAllTimeStamps() {
-        val db = FirebaseFirestore.getInstance()
-        val location = "dummy location"
-        val date : Date? = null
-
-        db.collection("appointments")
-            .whereEqualTo("mobile number", UserData.mobileNumber)
     }
 
     private fun getSavedDate(callback: (String?) -> Unit) {
@@ -226,12 +190,11 @@ class AppointmentScreenActivity : AppCompatActivity() {
             .whereEqualTo("mobile number", UserData.mobileNumber)
             .get()
             .addOnSuccessListener { query ->
+                var date : String? = null
                 if(!query.isEmpty) {
-                    val date = query.documents[0].getString("date")
-                    callback(date)
-                } else {
-                    //idk log smth?
+                    date = query.documents[0].getString("date")
                 }
+                callback(date)
             }
     }
 }
--- app/src/main/java/com/mobdeve/s11s13/group13/mp/vaccineph/helpers/UserData.kt
+++ app/src/main/java/com/mobdeve/s11s13/group13/mp/vaccineph/helpers/UserData.kt
@@ -2,6 +2,7 @@ package com.mobdeve.s11s13.group13.mp.vaccineph.helpers
 
 
 object UserData {
-    var mobileNumber : String = "+63939 919 1889"
-    var userDocumentId : String = ""
+    var mobileNumber = "+63939 919 1889"
+    var userDocumentId = ""
+    var location = "dummy location"
 }
