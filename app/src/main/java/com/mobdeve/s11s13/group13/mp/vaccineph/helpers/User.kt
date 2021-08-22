efc25be0ac25edfb06a3d946abb3bb9b61a73da7
UyyyL <74406287+UyyyL@users.noreply.github.com>
2021-08-22 15:27:05 +0800

    Update User.kt

--- app/src/main/java/com/mobdeve/s11s13/group13/mp/vaccineph/helpers/User.kt
+++ app/src/main/java/com/mobdeve/s11s13/group13/mp/vaccineph/helpers/User.kt
@@ -2,7 +2,7 @@ package com.mobdeve.s11s13.group13.mp.vaccineph.helpers
 
 
 object User {
-    var mobileNumber = "+63939 919 1889"
+    var mobileNumber = "+63917 844 2096"
     var location = "dummy location"
     var isRegistered = false;
 }
